1cec88651b566209625b6a1ddca6462a9adaf545
Altmertrick <altmertricks@gmail.com>
2023-01-26 21:01:26 +0200

    basic filters for getAllProducts

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,29 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error();
-  res.status(200).json({ data: 'testing products route' });
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, amount: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ data: 'all products' });
+  const { featured, company, name } = req.query;
+
+  const queryParams = {};
+
+  if (featured) {
+    queryParams.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryParams.company = company;
+  }
+  if (name) {
+    //using mongo db query operator regex for a filter
+    queryParams.name = { $regex: name, $options: 'i' };
+  }
+
+  const products = await Product.find(queryParams);
+
+  res.status(200).json({ products, amount: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
