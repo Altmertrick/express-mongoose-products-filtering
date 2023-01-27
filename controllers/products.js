5c0d9579e623413bb4f7e7f0fe172817c7e8fea1
Altmertrick <altmertricks@gmail.com>
2023-01-27 21:55:40 +0200

    added sorting and selecting to get all task

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,46 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { company, name, price, sort, fields } = req.query;
+
+  const queryOject = {};
+  if (company) {
+    queryOject.company = company;
+  }
+  if (name) {
+    //using mongo db query operator regex for a filter
+    queryOject.name = { $regex: name, $options: 'i' };
+  }
+  if (price) {
+    queryOject.price = Number(price);
+  }
+
+  let result = Product.find(queryOject);
+  //Sort
+  if (sort) {
+    // ...api/v1/products?sort=price,-name
+    // 'price,-name,date' => 'price -name date'
+    // result.sort('price -name') or sort({price: 1, name: -1})
+    console.log(sort);
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  //Select
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
+
   res.status(200).json({ products, amount: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryParams = {};
 
@@ -17,11 +51,23 @@ const getAllProducts = async (req, res) => {
     queryParams.company = company;
   }
   if (name) {
-    //using mongo db query operator regex for a filter
     queryParams.name = { $regex: name, $options: 'i' };
   }
 
-  const products = await Product.find(queryParams);
+  let result = Product.find(queryParams);
+  //Sort
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  //Select
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+  const products = await result;
 
   res.status(200).json({ products, amount: products.length });
 };
