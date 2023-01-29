807e6bc43383351c7de7df0200439e20ab4685f8
Altmertrick <altmertricks@gmail.com>
2023-01-29 10:55:34 +0200

    added pagination to getAllTask contr

--- controllers/products.js
+++ controllers/products.js
@@ -3,6 +3,7 @@ const Product = require('../models/product');
 const getAllProductsStatic = async (req, res) => {
   const { company, name, price, sort, fields } = req.query;
 
+  //use query object filter to find documents by fields' values
   const queryOject = {};
   if (company) {
     queryOject.company = company;
@@ -21,23 +22,31 @@ const getAllProductsStatic = async (req, res) => {
     // ...api/v1/products?sort=price,-name
     // 'price,-name,date' => 'price -name date'
     // result.sort('price -name') or sort({price: 1, name: -1})
-    console.log(sort);
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('createdAt');
   }
 
-  //Select
+  //Select - returns documents with specific fields
   if (fields) {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
 
+  //Pagination - implementing pagination using skip and limit methods
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
 
-  res.status(200).json({ products, amount: products.length });
+  res.status(200).json({ amount: products.length, products });
 };
+
+/////
 
 const getAllProducts = async (req, res) => {
   const { featured, company, name, sort, fields } = req.query;
@@ -67,9 +76,17 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
+
+  //Pagination
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
 
-  res.status(200).json({ products, amount: products.length });
+  res.status(200).json({ amount: products.length, products });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
