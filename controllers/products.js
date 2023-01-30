0cb504eb8f3162c0795f21a1dfbe1370162f428b
Altmertrick <altmertricks@gmail.com>
2023-01-30 15:00:29 +0200

    added numberic filtering

--- controllers/products.js
+++ controllers/products.js
@@ -1,22 +1,58 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const { company, name, price, sort, fields } = req.query;
+  const { company, name, price, sort, fields, numericFilters } = req.query;
 
   //use query object filter to find documents by fields' values
-  const queryOject = {};
+  const queryObject = {};
   if (company) {
-    queryOject.company = company;
+    queryObject.company = company;
   }
   if (name) {
-    //using mongo db query operator regex for a filter
-    queryOject.name = { $regex: name, $options: 'i' };
+    //using mongo db query operator - regex for a filter
+    queryObject.name = { $regex: name, $options: 'i' };
   }
   if (price) {
-    queryOject.price = Number(price);
+    queryObject.price = Number(price);
   }
 
-  let result = Product.find(queryOject);
+  if (numericFilters) {
+    //Numeric filters - allow user to get documents based on condition
+    //api/v1/products?numericFilters=price>30,rating>=4
+    //we need to format numericFilters to query object dynamically
+    //numericFilters = 'price>20,rating>=3.5' to
+    //queryObject = { price: {$lh: 30}, rating: {$gh: 4} }
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+    const regEx = /\b(>|>=|=|<|<=)\b/g;
+
+    const filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    // console.log(1, numericFilters);
+    // console.log('------');
+    // console.log(2, filters);
+
+    //options that has number values in our documents in db
+    const options = ['price', 'rating'];
+
+    filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+    console.log(queryObject);
+  }
+
+  let result = Product.find(queryObject);
+
   //Sort
   if (sort) {
     // ...api/v1/products?sort=price,-name
@@ -43,13 +79,18 @@ const getAllProductsStatic = async (req, res) => {
 
   const products = await result;
 
-  res.status(200).json({ amount: products.length, products });
+  //Counting all products that match our filters
+  //it is necessary to create pagination of front-end
+  const count = await Product.countDocuments(queryObject);
+
+  res
+    .status(200)
+    .json({ allMatchedItems: count, amount: products.length, products });
 };
 
 /////
-
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
 
   const queryParams = {};
 
@@ -61,6 +102,30 @@ const getAllProducts = async (req, res) => {
   }
   if (name) {
     queryParams.name = { $regex: name, $options: 'i' };
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+    const regEx = /\b(>|>=|=|<|<=)\b/g;
+    const filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = ['price', 'rating'];
+    filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryParams[field] = { [operator]: Number(value) };
+      }
+    });
+    console.log(queryParams);
   }
 
   let result = Product.find(queryParams);
@@ -85,8 +150,9 @@ const getAllProducts = async (req, res) => {
   result = result.skip(skip).limit(limit);
 
   const products = await result;
+  const allMatchedItems = await Product.countDocuments(queryParams);
 
-  res.status(200).json({ amount: products.length, products });
+  res.status(200).json({ allMatchedItems, amount: products.length, products });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
